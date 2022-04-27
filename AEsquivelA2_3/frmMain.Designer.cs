ed5fbb8143246021d958448095b30d0cc3f4ddc7
Angel Esquivel Jr <104031869+AngelEsquivelJr@users.noreply.github.com>
2022-04-27 14:32:41 -0500

    Initial Commit

---
+++ AEsquivelA2_3/frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace AEsquivelA2_3
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgrBarSplash = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.bkgWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // pgrBarSplash
+            // 
+            this.pgrBarSplash.Location = new System.Drawing.Point(12, 93);
+            this.pgrBarSplash.Name = "pgrBarSplash";
+            this.pgrBarSplash.Size = new System.Drawing.Size(455, 23);
+            this.pgrBarSplash.TabIndex = 0;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(215, 148);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(35, 13);
+            this.lblProgress.TabIndex = 1;
+            this.lblProgress.Text = "label1";
+            // 
+            // bkgWorker
+            // 
+            this.bkgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bkgWorker_DoWork);
+            this.bkgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bkgWorker_ProgressChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 195);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pgrBarSplash);
+            this.Name = "frmMain";
+            this.Text = "Splash";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgrBarSplash;
+        private System.Windows.Forms.Label lblProgress;
+        private System.ComponentModel.BackgroundWorker bkgWorker;
+    }
+}
+
---
+++ AEsquivelA2_3/frmMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AEsquivelA2_3
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void bkgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            for(int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(50);
+                bkgWorker.ReportProgress(i);
+            }
+        }
+
+        private void bkgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgrBarSplash.Value = e.ProgressPercentage;
+            lblProgress.Text = "Progress: " + e.ProgressPercentage.ToString()+ "%";
+            if(pgrBarSplash.Value >= 100)
+            {
+                MessageBox.Show("Your application is open.");
+                //for a new form
+                //new formName().Show()
+                //this.Hide()
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //makes progress bar run
+            bkgWorker.WorkerReportsProgress = true;
+            bkgWorker.RunWorkerAsync();
+        }
+    }
+}
